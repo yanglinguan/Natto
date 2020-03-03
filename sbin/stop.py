985b9924d9be5333fe1c5475ba37167f70590d2e
yanglinguan <yanglinguan@gmail.com>
2020-03-03 11:12:29 -0500

    stop exp script

---
+++ sbin/stop.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+import argparse
+import json
+
+from paramiko import SSHClient, AutoAddPolicy
+
+arg_parser = argparse.ArgumentParser(description="stop exp.")
+
+# Cluster configuration file
+arg_parser.add_argument('-c', '--config', dest='config', nargs='?',
+                        help='configuration file', required=True)
+args = arg_parser.parse_args()
+
+# Reads configurations
+config_file = open(args.config, "r")
+config = json.load(config_file)
+config_file.close()
+
+
+def stop_servers():
+    for serverId, info in config["servers"].items():
+        ip = info["ip"]
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+        ssh.connect(ip)
+        cmd = "killall -9 carousel-server"
+        print(cmd + " # at " + ip)
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        print(stdout.read())
+        print(stderr.read())
+
+
+def stop_clients():
+    for clientId, info in config["clients"].items():
+        ip = info["ip"]
+        ssh = SSHClient()
+        ssh.set_missing_host_key_policy(AutoAddPolicy())
+        ssh.connect(ip)
+        cmd = "killall -9 client"
+        print(cmd + " # at " + ip)
+        stdin, stdout, stderr = ssh.exec_command(cmd)
+        print(stdout.read())
+        print(stderr.read())
+
+
+def main():
+    stop_clients()
+    stop_servers()
+
+
+if __name__ == "__main__":
+    main()
