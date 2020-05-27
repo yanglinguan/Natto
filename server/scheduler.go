772ed14f5b6c901aaa41d31fd40745c736a2de82
yanglinguan <yanglinguan@gmail.com>
2020-05-27 14:39:22 -0400

    debug

--- server/scheduler.go
+++ server/scheduler.go
@@ -51,8 +51,8 @@ func (ts *TimestampScheduler) run() {
 
 func conflict(low *ReadAndPrepareOp, high *ReadAndPrepareOp) bool {
 	//log.Warnf("find conflict txn %v txn %v", low, high)
-	log.Warnf("low txn %v readKey %v writeKey %v", low.txnId, low.allReadKeys, low.allWriteKeys)
-	log.Warnf("high txn %v readKey %v writeKey %v", high.txnId, high.allReadKeys, high.allWriteKeys)
+	//log.Warnf("low txn %v readKey %v writeKey %v", low.txnId, low.allReadKeys, low.allWriteKeys)
+	//log.Warnf("high txn %v readKey %v writeKey %v", high.txnId, high.allReadKeys, high.allWriteKeys)
 	for rk := range low.allReadKeys {
 		if _, exist := high.allWriteKeys[rk]; exist {
 			log.Debugf("key %v : txn (low) %v read and txn (high) %v write", rk, low.txnId, high.txnId)
@@ -93,12 +93,12 @@ func (ts *TimestampScheduler) checkConflictWithHighPriorityTxn(op *ReadAndPrepar
 		//lTm := time.Unix(op.request.Timestamp, 0)
 		duration := time.Duration(cur.score - op.request.Timestamp)
 		//duration := hTm.Sub(lTm)
-		log.Warnf("here1: high txn %v and low txn %v duration %v, %v %v", cur.v.(*ReadAndPrepareOp).txnId, op.txnId, duration)
+		//log.Warnf("here1: high txn %v and low txn %v duration %v, %v %v", cur.v.(*ReadAndPrepareOp).txnId, op.txnId, duration)
 		if duration <= ts.server.config.GetTimeWindow() {
-			log.Warnf("here2: txn %v within duration", op.txnId)
+			//log.Warnf("here2: txn %v within duration", op.txnId)
 			if conflict(op, cur.v.(*ReadAndPrepareOp)) {
-				log.Warnf("here3: txn %v self abort because of high priority txn %v",
-					op.txnId, cur.v.(*ReadAndPrepareOp).txnId)
+				//log.Warnf("here3: txn %v self abort because of high priority txn %v",
+				//	op.txnId, cur.v.(*ReadAndPrepareOp).txnId)
 				op.selfAbort = true
 				break
 			}
