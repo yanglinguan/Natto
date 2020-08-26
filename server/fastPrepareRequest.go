53668ec155fa7a224d0549f6c29ce1e75d0e1115
yanglinguan <yanglinguan@gmail.com>
2020-08-26 15:30:13 +0800

    debug

--- server/fastPrepareRequest.go
+++ server/fastPrepareRequest.go
@@ -20,9 +20,12 @@ func (p *FastPrepareRequestOp) Execute(coordinator *Coordinator) {
 	txnId := p.request.PrepareResult.TxnId
 	twoPCInfo := coordinator.initTwoPCInfoIfNotExist(txnId)
 	log.Debugf("txn %v receive fast prepared result from partition %v result %v",
-		txnId, p.request.PrepareResult.TxnId, p.request.PrepareResult.PrepareStatus)
+		txnId,
+		p.request.PrepareResult.PartitionId,
+		TxnStatus(p.request.PrepareResult.PrepareStatus).String())
 	if twoPCInfo.status.IsAbort() {
-		log.Debugf("txn %v is already abort abort reason %v", txnId, twoPCInfo.status.String())
+		log.Debugf("txn %v is already abort abort reason %v",
+			txnId, twoPCInfo.status.String())
 		return
 	}
 
@@ -38,7 +41,7 @@ func (p *FastPrepareRequestOp) Execute(coordinator *Coordinator) {
 
 	if pResult, exist := twoPCInfo.partitionPrepareResult[pId]; exist {
 		log.Debugf("txn %v partition %v has prepared result %v, isFastPrepare %v",
-			txnId, pId, twoPCInfo.partitionPrepareResult[pId].status,
+			txnId, pId, twoPCInfo.partitionPrepareResult[pId].status.String(),
 			twoPCInfo.partitionPrepareResult[pId].isFastPrepare)
 		if ok {
 			pResult.isFastPrepare = true
@@ -50,7 +53,7 @@ func (p *FastPrepareRequestOp) Execute(coordinator *Coordinator) {
 		log.Debugf("txn %v pId %v fast path not success yet", txnId, pId)
 		return
 	}
-	log.Debugf("txn %v pId %v fast path success", txnId, pId)
+	log.Debugf("txn %v pId %v fast path success, status %v", txnId, pId, status.String())
 	leaderPrepareRequest := twoPCInfo.fastPathPreparePartition[pId].leaderResult.request.PrepareResult
 	twoPCInfo.partitionPrepareResult[pId] = NewPartitionStatus(status, true, leaderPrepareRequest)
 	if status.IsAbort() {
