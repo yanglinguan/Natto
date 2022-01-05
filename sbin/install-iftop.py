6a836e2c511c369a4f1db385bc6e3d20fe635247
yanglinguan <yanglinguan@gmail.com>
2022-01-04 20:08:03 -0500

    script

--- sbin/install-iftop.py
+++ sbin/install-iftop.py
@@ -27,8 +27,9 @@ def main():
     threads = []
     machines_server = utils.parse_server_machine(config)
     machines_client = utils.parse_client_machine(config)
+    utils.create_ssh_client([machines_server, machines_client])
     install_cmd = "sudo apt-get install -y libpcap-dev; sudo apt-get install -y ncurses-dev; cd iftop-*; ./configure; make; sudo make install"
-    for ip, machine in machines_client.item():
+    for ip, machine in machines_client.items():
         thread = threading.Thread(
             target=ssh_exec_thread,
             args=(machine.get_ssh_client(), install_cmd)
@@ -37,7 +38,7 @@ def main():
         thread.start()
         print(install_cmd + " # at " + ip)
 
-    for ip, machine in machines_server.item():
+    for ip, machine in machines_server.items():
         thread = threading.Thread(
             target=ssh_exec_thread,
             args=(machine.get_ssh_client(), install_cmd)
